efbb31ae768ce155e566ad5d60270d6b3cc7eae3
Evgeniy Mitskevich <vem-89@mail.ru>
2020-06-18 18:07:41 +0300

    JS jQuery формы

---
+++ 15.06.2020/js.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var myEl = document.querySelectorAll("#myDiv");
+    console.log("Отработало событие DOMContentLoaded");
+})
+​
+window.onload = function() {
+    var myEl = document.querySelectorAll("#myDiv");
+    console.log("Отработало событие window.onload");
+};
+​
+// var myEl = document.querySelectorAll("#myDiv");
+console.log(document.querySelector("#my-Div"));
+console.log(myDiv);
---
+++ 15.06.2020/script.js
@@ -0,0 +1,16 @@
+var header = $("h3");
+console.log(header);
+​
+// var liEls = document.querySelectorAll("li");
+​
+// for (var i = 0; i < liEls.length; i++) {
+//     liEls[i].addEventListener("click", function() {
+//         console.log("Клик на li")
+//     })
+// }
+// var q = $("li");
+// q.on("click", function() {
+//     console.log("Обработчик клика jquery")
+// })
+​
+$(".fotorama").fn.fotorama();
